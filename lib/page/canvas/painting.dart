f5300f13f2b73cb0e9f3e38b985cad2e33756918
515074431 <515074431@qq.com>
2021-07-18 08:33:39 +0800

    保存图片到相册

--- lib/page/canvas/painting.dart
+++ lib/page/canvas/painting.dart
@@ -1,8 +1,6 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
-
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
@@ -191,23 +189,23 @@ class _CanvasPaintingState extends State<CanvasPainting> {
     );
   }
 
-  // Future<void> _save() async {
-  //   RenderRepaintBoundary boundary =
-  //       globalKey.currentContext.findRenderObject();
-  //   ui.Image image = await boundary.toImage();
-  //   ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-  //   Uint8List pngBytes = byteData.buffer.asUint8List();
+  Future<void> _save() async {
+    RenderRepaintBoundary boundary =
+        globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData ? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
 
-  //   //Request permissions if not already granted
-  //   if (!(await Permission.storage.status.isGranted))
-  //     await Permission.storage.request();
+    //Request permissions if not already granted
+    if (!(await Permission.storage.status.isGranted))
+      await Permission.storage.request();
 
-  //   final result = await ImageGallerySaver.saveImage(
-  //       Uint8List.fromList(pngBytes),
-  //       quality: 60,
-  //       name: "canvas_image");
-  //   print(result);
-  // }
+    final result = await ImageGallerySaver.saveImage(
+        Uint8List.fromList(pngBytes),
+        quality: 60,
+        name: "canvas_image");
+    print(result);
+  }
 
   List<Widget> fabOption() {
     return <Widget>[
