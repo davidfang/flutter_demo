584b7a2b4eda282750621ea6206aea2902bd0ebc
515074431 <515074431@qq.com>
2021-07-18 23:52:14 +0800

    美化基础组件列表显示

--- lib/page/basic_components.dart
+++ lib/page/basic_components.dart
@@ -17,18 +17,78 @@ class _BasicComponentsState extends State<BasicComponents> {
     return Container(
         child: ListView(
       children: [
-        ListTile(title: Text('canvas'), onTap: () => _onTap(CanvasTest())),
-        ListTile(
-            title: Text('CanvasPainting'),
-            onTap: () => _onTap(CanvasPainting())),
-            ListTile(title: Text('SignaturePainter'), onTap: () => _onTap(Signature())),
+        _listItem(
+            title: 'canvas',
+            subtitle: '初始canvas',
+            description: '简单的canvas Demo',
+            routeWidget: CanvasTest()),
+        _listItem(
+            title: 'Canvas Painting',
+            subtitle: 'Canvas画笔画板',
+            description: '可以设置画笔颜色、粗细、透明度等',
+            routeWidget: CanvasPainting()),
+        _listItem(
+            title: 'Canvas画板',
+            subtitle: '简单的Canvas画板',
+            description: '单纯的涂鸦板',
+            routeWidget: Signature()),
       ],
     ));
   }
 
-  _onTap(Widget canvasTest) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return canvasTest;
-    }));
+  Widget _listItem(
+      {required String title,
+      required String subtitle,
+      required String description,
+      required Widget routeWidget}) {
+    return Column(
+      children: [
+        Card(
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+          color: Colors.grey[200],
+          child: ListTile(
+              onTap: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return routeWidget;
+                  })),
+              isThreeLine: true,
+              title: Text(title, style: TextStyle(fontSize: 20)),
+              subtitle: Column(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Text(
+                        subtitle * 10,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.normal),
+                      )),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    IconButton(
+                        icon: Icon(Icons.slideshow),
+                        onPressed: () => Navigator.push(context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) {
+                              return routeWidget;
+                            }))),
+                    SizedBox(width: 2),
+                    IconButton(
+                        icon: Icon(Icons.code),
+                        onPressed: () => Navigator.push(context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) {
+                              return routeWidget;
+                            })))
+                  ])
+                ],
+              )),
+        ),
+        // Divider(
+        //   color: Colors.blue,
+        // )
+      ],
+    );
   }
 }
